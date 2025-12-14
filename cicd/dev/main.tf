d0d8236e18e1ba046b93c1c01ff5e61bcc334251
Jeff Bryner <jeff@jeffbryner.com>
2025-12-14 15:33:33 -0800

    reorg a bit

--- cicd/dev/main.tf
+++ cicd/dev/main.tf
@@ -1,15 +1,15 @@
 module "gcp_project_setup" {
-  source = "../../modules/gcp_project_setup"
+  source = "../modules/gcp_project_setup"
 
-  project_name      = var.project_name
-  github_org        = var.github_org
-  github_repo       = var.github_repo
-  default_region    = var.default_region
-  org_id            = var.org_id
-  folder_id         = var.folder_id
-  billing_account   = var.billing_account
-  project_labels    = var.project_labels
-  environment       = "dev"
-  branch_name       = "^dev$"
+  project_name        = var.project_name
+  github_org          = var.github_org
+  github_repo         = var.github_repo
+  default_region      = var.default_region
+  org_id              = var.org_id
+  folder_id           = var.folder_id
+  billing_account     = var.billing_account
+  project_labels      = var.project_labels
+  environment         = "dev"
+  branch_name         = "^dev$"
   cloudbuild_filename = "/cicd/dev/cloudbuild.yaml"
 }
--- cicd/prod/main.tf
+++ cicd/prod/main.tf
@@ -1,15 +1,15 @@
 module "gcp_project_setup" {
-  source = "../../modules/gcp_project_setup"
+  source = "../modules/gcp_project_setup"
 
-  project_name      = var.project_name
-  github_org        = var.github_org
-  github_repo       = var.github_repo
-  default_region    = var.default_region
-  org_id            = var.org_id
-  folder_id         = var.folder_id
-  billing_account   = var.billing_account
-  project_labels    = var.project_labels
-  environment       = "prod"
-  branch_name       = "^main$"
+  project_name        = var.project_name
+  github_org          = var.github_org
+  github_repo         = var.github_repo
+  default_region      = var.default_region
+  org_id              = var.org_id
+  folder_id           = var.folder_id
+  billing_account     = var.billing_account
+  project_labels      = var.project_labels
+  environment         = "prod"
+  branch_name         = "^main$"
   cloudbuild_filename = "/cicd/prod/cloudbuild.yaml"
 }
